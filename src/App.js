0c7400278cb3478a9269c4069ddf34fd97ba2e54
weiyang <weiyangwygoh@gmail.com>
2023-02-20 20:30:27 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Button, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "This is a sample todo",
+      completed: false
+    }
+  ])
+
+  const addTodo = ({ text }) => {
+    setTodos([{ text }, ...todos])
+  }
+
+  const markTodo = (idx) => {
+    todos[idx].completed = !completed
+    setTodos(todos)
+  }
+
+  const deleteTodo = (idx) => {
+    setTodos(todos.splice(idx, 1))
+  }
+
+  const FormTodo = ({ addTodo }) => {
+    const [value, setValue] = useState('')
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      if (!value) return;
+      addTodo(value)
+      setValue('')
+    }
+
+    return (
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label><b>Add Todo</b></Form.Label>
+          <Form.Control type='text' value={value} placeholder='Add New Todo' className='input' onChange={(e) => { setValue(e.target.value) }} />
+        </Form.Group>
+        <Button variant="primary mb-3" type="submit">Submit</Button>
+      </Form>
+    )
+  }
+
+  const Todo = ({ idx, todo, markTodo, deleteTodo }) => (
+    <div className="todo">
+      <span style={{ textDecoration: todo.completed ? "line-through" : "" }}>{todo.text}</span>
+      <div>
+        <Button variant="outline-success" onClick={() => markTodo(idx)}>✓</Button>
+        {' '}
+        <Button variant="outline-danger" onClick={() => deleteTodo(idx)}>✕</Button>
+      </div>
+    </div>
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h1 className="text-center mb-4">Todo List</h1>
+        <FormTodo addTodo={addTodo} />
+        <div>
+          {todos.map((todo, idx) => (
+            <Card>
+              <Card.Body>
+                <Todo
+                  key={idx}
+                  idx={idx}
+                  todo={todo}
+                  markTodo={markTodo}
+                  deleteTodo={deleteTodo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
